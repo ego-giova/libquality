490e7a82d27e6a4a06de2dfe6e5e37ebd05e93e2
Jacomo Giovanetti <jacomo.giovanetti@mblabs.com.br>
2020-11-28 17:44:28 -0300

    feat: add metrics code error object

--- src/utils/errors/business.js
+++ src/utils/errors/business.js
@@ -11,3 +11,7 @@ export default class BusinessError extends ExtendableError {
 export const LibraryCodeError = {
   LIBRARIES_NOT_FOUND: 'libraries_not_found',
 };
+
+export const MetricsCodeError = {
+  LIBRARIES_STATISTICS_NOT_FOUND: 'not_found_statistics_for_requested_libraries',
+};
