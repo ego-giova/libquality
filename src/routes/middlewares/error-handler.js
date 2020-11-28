6cccb2760df910f0c0995731783498d1639cdc18
Jacomo Giovanetti <jacomo.giovanetti@mblabs.com.br>
2020-11-28 17:35:07 -0300

    feat: improve error handler

--- src/routes/middlewares/error-handler.js
+++ src/routes/middlewares/error-handler.js
@@ -1,23 +1,35 @@
 import httpStatus from 'http-status';
 import Constants from '../../utils/constants';
 import BusinessError from '../../utils/errors/business';
+import ExternalAPIError from '../../utils/errors/external-api';
+import PersistenceError from '../../utils/errors/persistence';
 import LoggerManager from '../../utils/logger-manager';
 
 export default function ErrorHandler(err, req, res) {
   if (err.errors && err.errors.length > 0) {
     res.status(httpStatus.BAD_REQUEST).json({
-      error: err.errors.pop().msg,
+      code: err.errors.pop().msg,
     });
   } else if (err instanceof BusinessError) {
-    res.status(httpStatus.BAD_REQUEST).json({
-      error: err.code,
-      ...err.options,
+    const status = (err.options && err.options.status) || httpStatus.BAD_REQUEST;
+
+    res.status(status).json({
+      code: err.code,
+      // ...err.options,
+    });
+  } else if (err instanceof ExternalAPIError) {
+    res.status(err.errResponse.status).json({
+      code: err.code,
+      message: err.errResponse.message,
+    });
+  } else if (err instanceof PersistenceError) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: err.message,
     });
   } else {
     LoggerManager.serverInternalErrorLogger(err);
 
     if (Constants.env !== 'production') {
-      // res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ stack: err.stack, message: err.message, ...err });
       res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: err.message, ...err });
     } else {
       res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
